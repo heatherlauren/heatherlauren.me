e18c209818bb67578453dcd3bde9966492d2a255
heatherlauren <heatherlaurendavidson@gmail.com>
2016-09-06 15:37:33 +0100

    feat(portfolio): add GSAP animation to portfolio filter

--- js/portfolio-filter.js
+++ js/portfolio-filter.js
@@ -1,25 +1,27 @@
----
----
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  $('.category-container__category_all').click(function() {
-    $(".portfolio__post").hide('slow')
-    $(".portfolio__post").show('slow')
+  var $portfolio_post = $('.portfolio__post')
+  var $portfolio_post_branding = $('.portfolio__post-branding')
+  var $portfolio_post_print = $('.portfolio__post-print')
+  var $portfolio_post_web = $('.portfolio__post-web')
+
+  $('.category-container__category_all').click(function () {
+    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
+    TweenLite.to($portfolio_post, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
   })
 
-  $('.category-container__category_branding').click(function() {
-    $(".portfolio__post").hide('slow')
-    $(".portfolio__post-branding").show('slow')
+  $('.category-container__category_branding').click(function () {
+    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
+    TweenLite.to($portfolio_post_branding, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
   })
 
-  $('.category-container__category_print').click(function() {
-    $(".portfolio__post").hide('slow')
-    $(".portfolio__post-print").show('slow')
+  $('.category-container__category_print').click(function () {
+    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
+    TweenLite.to($portfolio_post_print, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
   })
 
-  $('.category-container__category_web').click(function() {
-    $(".portfolio__post").hide('slow')
-    $(".portfolio__post-web").show('slow')
+  $('.category-container__category_web').click(function () {
+    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
+    TweenLite.to($portfolio_post_web, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
   })
-
 })
