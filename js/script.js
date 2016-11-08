3a5d195a25248eaa7cedabeeeca0e06b9c90d639
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-08 15:44:07 +0000

    replace portfolio filtering script

--- js/script.js
+++ js/script.js
@@ -17,38 +17,10 @@ $( document ).ready(function() {
     })
   })
 
-  // Portfolio Filter
-  var $portfolio_post = $('.portfolio__post')
-  var $portfolio_post_branding = $('.portfolio__post-branding')
-  var $portfolio_post_print = $('.portfolio__post-print')
-  var $portfolio_post_web = $('.portfolio__post-web')
+// Portfolio Filter
 
-  $('.category-container__category_all').click(function () {
-    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
-    TweenLite.to($portfolio_post, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
-  })
-
-  $('.category-container__category_branding').click(function () {
-    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
-    TweenLite.to($portfolio_post_branding, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
-  })
-
-  $('.category-container__category_print').click(function () {
-    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
-    TweenLite.to($portfolio_post_print, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
-  })
-
-  $('.category-container__category_web').click(function () {
-    TweenLite.to($portfolio_post, 0.5, {opacity: 0, display: 'none', ease: Power1.easeOut})
-    TweenLite.to($portfolio_post_web, 0.5, {opacity: 1, display: 'flex', delay: 0.5, ease: Power1.easeIn})
-  })
-
-  // Category Container
-  $(document).ready(function () {
-    $('.category-container__category').click(function (event) {
-      $('a').removeClass('category-container__category--active')
-      $(this).addClass('category-container__category--active')
-    })
+  $(function(){
+    $(".portfolio").mixItUp();
   })
 
   // Navbar Highlighter
