d5dba6742959e5de8df550dd16855e40e339a80e
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-08 16:28:00 +0000

    update script for blog filter

--- js/script.js
+++ js/script.js
@@ -2,28 +2,21 @@
 ---
 $( document ).ready(function() {
 
-  // Blog Filter
-  var categories = {{ site.categories | jsonify }}
-
-  $('.category-container__category_all').click(function() {
-    $('.blog__post').hide(1200)
-    $('.blog__post').show(1200)
-  })
-
-  $.each( categories, function (key) {
-    $(`.category-container__category_${key}`).click(function() {
-      $('.blog__post').hide(1200)
-      $(`.blog__post-${key}`).show(1200)
-    })
-  })
-
-// Portfolio Filter
-
+  // Filters
   $(function(){
     $(".portfolio").mixItUp();
   })
 
+  $(function(){
+    $(".blog").mixItUp({
+      layout: {
+        display: 'flex'
+      }
+    });
+  })
+
   // Navbar Highlighter
+
   if (/blog/.test(window.location.href)) {
     $('.navbar__link--blog').addClass('navbar__link--active')
   } else if (/portfolio/.test(window.location.href)) {
@@ -31,6 +24,7 @@ $( document ).ready(function() {
   }
 
   // Console Log
+
   console.log("Hire me! Find my CV at http://heatherlauren.me/cv.pdf")
 
 })
